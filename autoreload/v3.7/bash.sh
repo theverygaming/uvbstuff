f038acc2f2c369ad7db9498fb7f4ab496f11842a
Mircas001 <79532341+Mircas001@users.noreply.github.com>
2025-05-26 16:57:51 -0300

    Translate bash.sh

--- autoreload/v3.7/bash.sh
+++ autoreload/v3.7/bash.sh
@@ -2,19 +2,19 @@
 
 set -e
 
-# Cria o ambiente virtual, se não existir
+#Creates venv in case it does not exist
 if [ ! -d "autoreload_venv" ]; then
-  python3 -m venv autoreload_venv || { echo "Erro ao criar o venv"; exit 1; }
+  python3 -m venv autoreload_venv || { echo "Error creating venv"; exit 1; }
 fi
 
-# Ativa o ambiente virtual
-source autoreload_venv/bin/activate || { echo "Erro ao ativar o venv"; exit 1; }
+#Activates venv
+source autoreload_venv/bin/activate || { echo "Error activating venv"; exit 1; }
 
-# Instala os pacotes
+#Install packages
 pip install git+https://github.com/theverygaming/silly.git@050482f272af4fab564c15238bd7b17ecf22197d || { echo "Erro ao instalar silly"; exit 1; }
-pip install requests || { echo "Erro ao instalar requests"; exit 1; }
+pip install requests || { echo "Error installing requests"; exit 1; }
 
-# Loop de execução
+# Loop 
 while true; do
   python main.py >> autoreload.log 2>&1
 done
