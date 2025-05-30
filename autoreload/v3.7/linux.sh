d4b4bba069ea7ad8fb89d3f7f6a06261151f2267
Mircas001 <79532341+Mircas001@users.noreply.github.com>
2025-05-30 17:29:04 -0300

    Rename bash.sh to linux.sh + translations

--- autoreload/v3.7/linux.sh
+++ autoreload/v3.7/linux.sh
@@ -11,7 +11,7 @@ fi
 source autoreload_venv/bin/activate || { echo "Error activating venv"; exit 1; }
 
 #Install packages
-pip install git+https://github.com/theverygaming/silly.git@050482f272af4fab564c15238bd7b17ecf22197d || { echo "Erro ao instalar silly"; exit 1; }
+pip install git+https://github.com/theverygaming/silly.git@050482f272af4fab564c15238bd7b17ecf22197d || { echo "Error installing silly"; exit 1; }
 pip install requests || { echo "Error installing requests"; exit 1; }
 
 # Loop 
