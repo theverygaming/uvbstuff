6a1b3f52833d73e947f02f14e157f024685434e0
Mircas001 <79532341+Mircas001@users.noreply.github.com>
2025-06-14 11:30:56 -0300

    Added stdout, switched to requirements.txt.

--- autoreload/v3.7/linux.sh
+++ autoreload/v3.7/linux.sh
@@ -4,16 +4,19 @@ set -e
 
 #Creates venv in case it does not exist
 if [ ! -d "autoreload_venv" ]; then
+  echo "Venv not found! Creating venv..."
   python3 -m venv autoreload_venv || { echo "Error creating venv"; exit 1; }
 fi
 
 #Activates venv
+echo "Activating venv..."
 source autoreload_venv/bin/activate || { echo "Error activating venv"; exit 1; }
 
 #Install packages
-pip install git+https://github.com/theverygaming/silly.git@050482f272af4fab564c15238bd7b17ecf22197d || { echo "Error installing silly"; exit 1; }
-pip install requests || { echo "Error installing requests"; exit 1; }
+echo "Checking and installing required packages..."
+pip install -r requirements.txt --upgrade-strategy only-if-needed || { echo "Error installing requirements"; exit 1; }
 
+echo "Starting silly"
 # Loop 
 while true; do
   python main.py >> autoreload.log 2>&1
